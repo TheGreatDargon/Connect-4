eeeca726328dd854c67f93f010bf2be40571819c
GreatDargon <128763680+TheGreatDargon@users.noreply.github.com>
2025-09-27 15:34:08 -0500

    Added connect4 python source file and readme

---
+++ connect4.py
@@ -0,0 +1,115 @@
+import numpy as np
+
+cols = 7
+rows = 6
+connect = 4  # Connections required to win
+
+theBoard = np.array([[" 0 " for i in range(cols)] for j in range(rows)])
+
+def printBoard():
+    for i in range(0,6):
+        print(theBoard[i])
+
+def is_Empty(row, col):
+    if theBoard[row][col] == " 0 ":
+        return True
+    else:
+        return False
+
+def is_Winner(piece):
+
+    # Check Rows
+    for r in range(rows):
+        for c in range (cols - connect + 1):
+            if (theBoard[r][c] == piece and
+                theBoard[r][c+1] == piece and
+                theBoard[r][c+2] == piece and
+                theBoard[r][c+3] == piece):
+                return True
+        
+    # Check Columns
+    for c in range(cols):
+        for r in range (rows - connect + 1):
+            if (theBoard[r][c] == piece and
+                theBoard[r+1][c] == piece and
+                theBoard[r+2][c] == piece and
+                theBoard[r+3][c] == piece):
+                return True
+            
+    # Check Diagonals 
+    # up right
+    for r in range(rows - connect + 1):
+        for c in range(rows - connect + 1):
+            if (theBoard[r][c] == piece and
+                theBoard[r+1][c+1] == piece and
+                theBoard[r+2][c+2] == piece and
+                theBoard[r+3][c+3] == piece):
+                return True
+    # down right
+    for r in range(rows - connect + 1):
+        for c in range(connect-1,cols):
+            if (theBoard[r][c] == piece and
+                theBoard[r+1][c-1] == piece and
+                theBoard[r+2][c-2] == piece and
+                theBoard[r+3][c-3] == piece):
+                return True
+            
+    return False    
+def makeMove(player, move):
+    if move < 0 or move > 6:
+        print("You didn't put the piece in a column, Try again.")
+        return False
+    for i in range(rows-1, -1, -1):
+        if is_Empty(i, move) == True:
+            theBoard[i][move] = player
+            return True
+            break
+
+def onePlayer():
+    player1 = " R "
+    comp = " Y "
+
+def twoPlayer():
+    player1 = " R "
+    player2 = " Y "
+
+    moved = False
+
+    # ! Infinite loop for now until I implement checking for a winner !
+    while is_Winner(player1) is False or is_Winner(player2) is False:
+        move = input("Player 1 choose a column to place a piece: ")
+        while makeMove(player1, int(move)) != True:
+            move = input("Player 1 choose a column to place a piece: ")
+            print(f"{player1}, That column is full, please choose another column.")
+            continue
+        printBoard()
+        if is_Winner(player1) is True:
+            print(f"{player1} wins!")
+            return 0
+        move = input("Player 2 choose a column to place a piece: ")
+        while makeMove(player2, int(move)) != True:
+            move = input("Player 2 choose a column to place a piece: ")
+            print(f"{player2}, That column is full, please choose another column.")
+            continue
+        printBoard()
+        if is_Winner(player2) is True:
+            print(f"{player2} wins!")
+            return 0
+        
+
+
+
+def main():
+    choice = 0
+    while choice == 0:
+        choice = int(input("Welcome to Connect-4, Please choose a game mode:\n1. One Player\n2. Two Player\n3. Quit\n"))
+        if choice == 1:
+            onePlayer()
+        elif choice == 2:
+            twoPlayer()
+        else:
+            return 0
+
+
+
+main()
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Introduction
+
+Objective: The goal of this project is to build a two-player version of the classic game [Connect Four](https://www.calculators.org/games/connect-4/).
+
+Connect Four is played on a vertical grid of 6 rows and 7 columns. Two players take turns dropping one of their colored pieces into a column. The piece falls to the lowest available empty space within that column. The first player to get four of their pieces in a row—horizontally, vertically, or diagonally—wins the game. If the entire grid is filled and no player has won, the game is a draw.
+
+In the next section I will go over my To-Do list. I will also provide a short description on how I am acomplishing this. And if I have any ideas for improvement which can be revisted later.
+
+# To-Do
+
+[X] Create Board
+[X] Display the Board
+[X] Allow player to place Pieces
+[X] Check if a slot is empty
+[X] Implement "Gravity" to move pieces to bottom of column
+[X] Check if a column is full
+[X] Allow for 2 players
+[X] Create win condition logic
+    [X] Create row win logic
+    [X] Create col win logic
+    [X] Create Diag win logic
+[] Create a simple computer algorithm for one Player
